8306291a0151c6f54310b2701ea03af6d5185752
Sanaz.darbandi <mahta17rw@gmail.com>
2024-10-28 01:06:40 +0330

    1-array

---
+++ 1_array/1_array/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Please enter the Fibonacci sequence number (n): ");
+        int n = int.Parse(Console.ReadLine());
+
+        if (n < 0)
+        {
+            Console.WriteLine("The entered number must be non-negative.");
+            return;
+        }
+
+        long[] fib = new long[n + 1];
+
+        fib[0] = 0;
+        if (n > 0)
+        {
+            fib[1] = 1;
+        }
+
+        for (int i = 2; i <= n; i++)
+        {
+            fib[i] = fib[i - 1] + fib[i - 2];
+        }
+
+        Console.WriteLine($"The {n}th sequence of the Fibonacci series: { fib[n]}");
+    }
+}
